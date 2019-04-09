b6519523f1cf49771122c5d865a10df93a0a1903
musicprogram <juan-o-henao@hotmail.es>
2019-04-09 13:12:10 -0500

    iniciando proyecto , con webpack

---
+++ src/init.js
@@ -0,0 +1,25 @@
+const config = { 
+	width: 320,
+	height: 180,
+	parent: "container",
+	type: Phaser.AUTO,
+	scene: {
+		preload: preload,
+		create: create,
+		update: update
+	}
+}
+
+let game = new Phaser.Game(config);
+
+function preload(){
+
+}
+
+function create(){
+
+}
+
+function update(){
+	
+}
